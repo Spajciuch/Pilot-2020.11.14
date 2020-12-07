263c1ad7e61753503dbad66c58809989c176b7c2
Spyte <spyte@pm.me>
2020-12-07 23:28:50 +0100

    Bugged ghost arrived

--- Pilot 2020.11.14/Assets/Scripts/Edek.cs
+++ Pilot 2020.11.14/Assets/Scripts/Edek.cs
@@ -7,9 +7,14 @@ public class Edek : MonoBehaviour
     public float range, speed;
     
     public GameObject target;
+    private GameObject player, detector;
     private GameActions game;
+    private EdgeCollider2D detectorCollider;
+    private PolygonCollider2D ownBody;
 
     Vector2 highiestPosition, lowerPosition;
+
+    [SerializeField] private LayerMask playerLayerMask;
 
     void Start()
     {
@@ -17,19 +22,40 @@ public class Edek : MonoBehaviour
         lowerPosition = new Vector2(transform.position.x, transform.position.y - range);
 
         game = gameObject.AddComponent<GameActions>();
+        
+        player = GameObject.Find("Player");
+        detector = GameObject.Find("PlayerDetector");
+        detectorCollider = detector.GetComponent<EdgeCollider2D>();
+        ownBody = GetComponent<PolygonCollider2D>();
     }
 
 
     void FixedUpdate()
     {
-        Movement();   
+        Movement();
+        // PlayerInteractions();
     }
 
     private void Movement()
     {
+        if (target)
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x +0.5f, target.transform.position.y + 0.5f);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+
         if (target == null)
         {
             game.Float(gameObject, highiestPosition, lowerPosition, speed);
         }
     }
+
+    private void PlayerInteractions()
+    {
+        if (ownBody.IsTouchingLayers(playerLayerMask))
+        {
+            game.Kill(target, true, true, false);
+        }
+    }
+
 }
---
+++ Pilot 2020.11.14/Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    private Edek edek;
+
+    private void Update()
+    {
+        edek = FindObjectOfType<Edek>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        edek.target = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        edek.target = null;
+    }
+}
